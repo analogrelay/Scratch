9bbff3620c671024ae1412a0ad44939a37aa2584
Andrew Stanton-Nurse <andrew@andrewnurse.net>
2015-10-09 15:24:08 -0700

    diversify the test a bit

--- SocketTest/Program.cs
+++ SocketTest/Program.cs
@@ -11,10 +11,56 @@ namespace SocketTest
 {
     public class Program
     {
-        public void Main(string[] args)
+        public void Main(string[] args) {
+            if(args.Length < 1) {
+                Console.Error.WriteLine("Specify a url as the argument");
+            }
+
+            Console.WriteLine("=== Testing with raw Stream ===");
+            try {
+                TestStream(args[0]);
+            } catch(Exception ex) {
+                Console.WriteLine("FAILED:");
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=== Testing with StreamReader ===");
+            try {
+                TestStreamReader(args[0]);
+            } catch(Exception ex) {
+                Console.WriteLine("FAILED:");
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void TestStreamReader(string urlStr)
         {
             // Make a simple HTTP request
-            var url = new Uri(args[0]);
+            var url = new Uri(urlStr);
+
+            var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(url.Host, url.Port);
+
+            using (var stream = GetStream(socket, url))
+            {
+                // Send the request
+                var request = $"GET {url.PathAndQuery} HTTP/1.1\r\nHost: {url.Host}\r\nConnection: close\r\n\r\n";
+                var buffer = Encoding.UTF8.GetBytes(request);
+                stream.Write(buffer, 0, buffer.Length);
+
+                // Read the response
+                using(var reader = new StreamReader(stream, Encoding.UTF8))
+                {
+                    Console.WriteLine(reader.ReadToEnd());
+                }
+            }
+        }
+
+        public void TestStream(string urlStr)
+        {
+            // Make a simple HTTP request
+            var url = new Uri(urlStr);
 
             var socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
             socket.Connect(url.Host, url.Port);
